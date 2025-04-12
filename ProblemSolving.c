d6a61e4d6e4212e3faf7930a1b263d50d70d5e63
Md-Ashikur-Rahman-Ashik <md.ashikur.upwork@gmail.com>
2025-04-12 22:32:32 +0600

    Solved second problem of codeforces

--- ProblemSolving.c
+++ ProblemSolving.c
@@ -17,3 +17,23 @@
 
 //     return 0;
 // }
+
+// #include <stdio.h>
+
+// int main()
+// {
+//     int num1, num2;
+
+//     scanf("%d %d", &num1, &num2);
+
+//     if (num1 % num2 == 0 || num2 % num1 == 0)
+//     {
+//         printf("Multiples");
+//     }
+//     else
+//     {
+//         printf("No Multiples");
+//     }
+
+//     return 0;
+// }

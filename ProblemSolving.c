2594ea332588c9d032111d1b795eb03074fded5f
Md-Ashikur-Rahman-Ashik <md.ashikur.upwork@gmail.com>
2025-04-13 22:55:28 +0600

    Successfully solved sixth codeforces problem

--- ProblemSolving.c
+++ ProblemSolving.c
@@ -139,44 +139,111 @@ int main()
 
     scanf("%d %d %d", &num1, &num2, &num3);
 
+    if (num1 == num2 || num1 == num3 || num2 == num3)
+    {
+        if (num1 == num2)
+        {
+            if (num1 < num3)
+            {
+                printf("%d ", num1);
+            }
+            else
+            {
+                printf("%d", num3);
+            }
+        }
+        else if (num2 == num3)
+        {
+            /* code */
+            if (num2 < num1)
+            {
+                printf("%d ", num2);
+            }
+            else
+            {
+                printf("%d ", num1);
+            }
+        }
+        else if (num1 == num3)
+        {
+            /* code */
+            if (num1 < num2)
+            {
+                /* code */
+                printf("%d ", num1);
+            }
+            else
+            {
+                printf("%d ", num2);
+            }
+        }
+    }
+
+    if (num1 == num2 || num1 == num3 || num2 == num3)
+    {
+        if (num1 == num2)
+        {
+            if (num1 > num3)
+            {
+                printf("%d", num1);
+            }
+            else
+            {
+                printf("%d", num3);
+            }
+        }
+        else if (num2 == num3)
+        {
+            /* code */
+            if (num2 > num1)
+            {
+                printf("%d", num2);
+            }
+            else
+            {
+                printf("%d", num1);
+            }
+        }
+        else if (num1 == num3)
+        {
+            /* code */
+            if (num1 > num2)
+            {
+                /* code */
+                printf("%d", num1);
+            }
+            else
+            {
+                printf("%d", num2);
+            }
+        }
+        return 0;
+    }
+
     if (num1 < num2 && num1 < num3)
     {
         printf("%d ", num1);
-
-        if (num2 > num1 && num2 > num3)
-        {
-            printf("%d", num2);
-        }
-        else
-        {
-            printf("%d", num3);
-        }
     }
     else if (num2 < num1 && num2 < num3)
     {
         printf("%d ", num2);
-
-        if (num1 > num2 && num1 > num3)
-        {
-            printf("%d", num1);
-        }
-        else
-        {
-            printf("%d", num3);
-        }
     }
     else
     {
         printf("%d ", num3);
+    }
 
-        if (num1 > num2 && num1 > num3)
-        {
-            printf("%d", num1);
-        }
-        else
-        {
-            printf("%d", num2);
-        }
+    if (num1 > num2 && num1 > num3 && num1 != num2 && num1 != num3)
+    {
+        printf("%d", num1);
+    }
+    else if (num2 > num1 && num2 > num3)
+    {
+        printf("%d", num2);
+    }
+    else
+    {
+        printf("%d", num3);
     }
 
     return 0;
